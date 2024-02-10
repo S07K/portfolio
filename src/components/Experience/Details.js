e71b66db6050c6d43e86e552233e90227429c3c8
shubham.k <shubham.k@VCX-LP-43.VCLOUDX.LAN>
2024-02-10 20:11:04 +0530

    added more content in experience and about section

--- src/components/Experience/Details.js
+++ src/components/Experience/Details.js
@@ -4,7 +4,6 @@ import Reveal from "../Reveal"
 
 const content_2023 = <div className={style.expWrapper}>
     <div className={style.detailsWrapper}>
-        {/* <h1 className={style.expTitle}>Got promoted</h1> */}
         <div className={style.details}>
             <Reveal>
                 <h3 className={style.expCompany}>EnableX</h3>
@@ -20,9 +19,50 @@ const content_2023 = <div className={style.expWrapper}>
                     <b>Skills:</b> Zoho <b>·</b> Zoho Extension <b>·</b> UI development <b>·</b> Design <b>·</b> User Experience Design (UED) <b>·</b> User Experience (UX) <b>·</b> User Interface Design <b>·</b> Git <b>·</b> jQuery <b>·</b> React <b>·</b> Redux <b>·</b> TypeScript
                 </Reveal>
                 <br></br>
-                <Reveal>
-                    <b>Description:</b> In my current capacity, I have independently developed a ZOHO Extension by seamlessly integrating the company's messaging API services. This project underscores my self-reliant and autonomous approach to project execution, as I took sole responsibility for its development without the involvement of any other team members. Furthermore, I presently serve as a valued member of the React development team, contributing to the collective effort and success of our projects.
-                </Reveal>
+                <b>Description:</b> 
+                <ul>
+                    <Reveal>
+                        <li>
+                            <b>Integration in the Chatbot Flow builder Application: </b> 
+                            Successfully integrated third party system APIs in the Application, allowing users to seamlessly
+                            push chatbot user data into CRM systems such as ZOHO, Zendesk, and HubSpot. Enhanced the functionality of the Builder by enabling direct data
+                            integration with popular CRM tools which covers 80% of the market, streamlining customer data management processes.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>Frontend Lead (Chatbot Builder SaaS Project): </b> 
+                            Led the front-end development of a Chatbot Builder SaaS Project, playing a pivotal role in ensuring the
+                            delivery of highly effective solutions. Spearheaded the coordination of design and development efforts within the frontend team, fostering collaboration
+                            to create a user-centric interface for the Chatbot Builder application.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>SaaS Extension Development Leader (ZOHO Marketplace): </b> 
+                            Independently conceptualized, designed, and built a bespoke SaaS extension for the
+                            ZOHO Marketplace. Placed special emphasis on SMS API integration, enhancing the extension's functionality and providing a flawless experience for
+                            users. Solely developed both the user interface (UI) and user experience (UX) for the extension, showcasing comprehensive skills in design and
+                            technical proficiency.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>API Integration Leadership (SaaS React Application): </b> 
+                            Demonstrated proficiency in leading API integration efforts within a SaaS React application,
+                            elevating its overall functionality. Collaborated closely with backend teams, ensuring effective communication between components, and contributing
+                            to a streamlined user experience.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>Optimized User Flow for React Project: </b> 
+                            Played a key role in refining the user flow for a React project, ensuring a smooth and intuitive experience for
+                            end-users. Identified specific challenges and pain points, strategically implementing improvements to enhance overall usability and elevate user
+                            satisfaction.
+                        </li>
+                    </Reveal>
+                </ul>
             </div>
         </div>
     </div>
@@ -30,7 +70,6 @@ const content_2023 = <div className={style.expWrapper}>
 
 const content_2022 = <div className={style.expWrapper}>
     <div className={style.detailsWrapper}>
-        {/* <h1 className={style.expTitle}>Started Corporate life</h1> */}
         <div className={style.details}>
             <Reveal><h3 className={style.expCompany}>EnableX</h3></Reveal>
             <Reveal><p className={style.expDesignation}>Engineer Trainee</p></Reveal>
@@ -38,23 +77,44 @@ const content_2022 = <div className={style.expWrapper}>
             <div className={style.expBrief}>
                 <Reveal><b>Skills:</b> UI development <b><b>·</b></b> Design <b>·</b> User Experience Design (UED) <b>·</b> User Experience (UX) <b>·</b> User Interface Design <b>·</b> Git <b>·</b> Web Services API <b>·</b> SASS <b>·</b> HTML5 <b>·</b> Cascading Style Sheets (CSS) <b>·</b> Bootstrap <b>·</b> JavaScript <b>·</b> jQuery</Reveal>
                 <br></br>
-                <Reveal><b>Description:</b> Within my role, I am proficient in JavaScript for managing the UI in accordance with API responses. Moreover, I play a key role in the software's development by implementing new features and enhancing its functionality. Additionally, I take full ownership of the integrated features, ensuring their successful integration and optimal performance.</Reveal>
+                <b>Description:</b>
+                <ul>
+                    <Reveal>
+                        <li>
+                            <b>Proficient JavaScript Developer: </b>
+                            Possess rich experience in JavaScript, leveraging its capabilities to create exceptional web applications. Revamped
+                            user interfaces by leveraging JavaScript's capabilities, resulting in streamlined workflows and improved user satisfaction.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>UX Enhancement Specialist: </b>
+                            Established expertise in optimizing user satisfaction through strategic UX improvements, resulting in a remarkable 80%
+                            increase in user engagement. Led feature development initiatives within SaaS products, ensuring a user-centric approach and enhancing overall user
+                            experience.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>API Integration Expert: </b>
+                            Showcased proficiency as an API integration expert, seamlessly incorporating third-party APIs to achieve responsive UI and
+                            real-time data functionalities.
+                        </li>
+                    </Reveal>
+                    <Reveal>
+                        <li>
+                            <b>Ruby Application Server Developer: </b>
+                            Successfully built the 1st Ruby application server within a challenging two-day timeframe. Showcased versatility
+                            and rapid acquisition of skills by becoming robust in a previously unfamiliar programming language for efficient server-side development.
+                        </li>
+                    </Reveal>
+                </ul>
             </div>
         </div>
     </div>
-    <br></br>
-    {/* <div>
-        <h1 className={style.expTitle}>Graduated</h1>
-        <div className={style.details}>
-            <h3 className={style.expCompany}>University of Delhi</h3>
-            <p className={style.expDesignation}>B.Sc. (Hons) Computer Science</p>
-            <p className={style.expDuration}>July 4<sup>th</sup> 2022 - July 4<sup>th</sup> 2023</p>
-        </div>
-    </div> */}
 </div>
 
 const content_2021 = <div className={style.expWrapper}>
-    {/* <h1 className={style.expTitle}>Got promoted</h1> */}
     <div className={style.detailsWrapper}>
         <div className={style.details}>
             <Reveal><h3 className={style.expCompany}>Teach For India</h3></Reveal>
@@ -62,6 +122,27 @@ const content_2021 = <div className={style.expWrapper}>
             <Reveal><p className={style.expDuration}>May 2021 - June 2021</p></Reveal>
             <div className={style.expBrief}>
                 <Reveal><b>Skills:</b> Design <b>·</b> User Experience Design (UED) <b>·</b> User Experience (UX) <b>·</b> User Interface Design <b>·</b> Git <b>·</b> Web Design</Reveal>
+                <br></br>
+                <Reveal>
+                    <b>Description:</b>
+                    <ul>
+                        <Reveal>
+                            <li>
+                                <b>Web Interface Development: </b>
+                                Designed and constructed a user-friendly web interface for a high-impact campaign, emphasizing both visual appeal and
+                                functional efficiency. Ensured the online presence aligned with campaign objectives, providing an engaging and effortless experience for users.
+                            </li>
+                        </Reveal>
+                        <Reveal>
+                            <li>
+                                <b>Design Layout Proficiency: </b>
+                                Acquired valuable expertise in crafting effective design layouts for webpages, enhancing the visual appeal and overall user
+                                experience of online content. Demonstrated a keen eye for design principles, contributing to the creation of visually compelling and intuitive web
+                                layouts.
+                            </li>
+                        </Reveal>
+                    </ul>
+                </Reveal>
             </div>
         </div>
     </div>
@@ -73,7 +154,17 @@ const content_2021 = <div className={style.expWrapper}>
             <div className={style.expBrief}>
                 <Reveal><b>Skills:</b> Git</Reveal>
                 <br></br>
-                <Reveal><b>Description:</b> Created visually appealing website loaders that offer effortless integration across various applications.</Reveal>
+                <Reveal>
+                    <b>Description:</b>
+                    <ul>
+                        <li>
+                            <b>Open-Source Involvement: </b>
+                            Contributed actively to open-source projects, gaining valuable experience in collaborative software development.
+                            Represented a commitment to community engagement, participating in the broader development community and contributing to the advancement of
+                            shared knowledge and resources.
+                        </li>
+                    </ul>
+                </Reveal>
             </div>
         </div>
     </div>
@@ -81,7 +172,6 @@ const content_2021 = <div className={style.expWrapper}>
 
 const content_2020 = <div className={style.expWrapper}>
     <div className={style.detailsWrapper}>
-        {/* <h1 className={style.expTitle}>Got promoted</h1> */}
         <div className={style.details}>
             <Reveal><h3 className={style.expCompany}>Started learning Web development</h3></Reveal>
             <Reveal><p className={style.expDesignation}>Got Certififications</p></Reveal>
@@ -95,7 +185,6 @@ const content_2020 = <div className={style.expWrapper}>
 
 const content_2019 = <div className={style.expWrapper}>
     <div className={style.detailsWrapper}>
-        {/* <h1 className={style.expTitle}>Got promoted</h1> */}
         <div className={style.details}>
             <Reveal><h3 className={style.expCompany}>Graduated From High School</h3></Reveal>
             <Reveal><p className={style.expDesignation}>Science Student</p></Reveal>
@@ -105,7 +194,6 @@ const content_2019 = <div className={style.expWrapper}>
         </div>
     </div>
     <div className={style.detailsWrapper}>
-        {/* <h1 className={style.expTitle}>Got promoted</h1> */}
         <div className={style.details}>
             <Reveal><h3 className={style.expCompany}>Got into College</h3></Reveal>
             <Reveal><p className={style.expDesignation}>B.Sc. (Hons.) Computer Science</p></Reveal>
